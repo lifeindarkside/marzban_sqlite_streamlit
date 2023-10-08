56a0443e291d657d40d390809af6a7fcd869717e
Elshad Guseynov <66727826+lifeindarkside@users.noreply.github.com>
2023-10-08 19:43:44 +0300

    Delete analytics directory

--- analytics/passwordhash.py
+++
@@ -1,8 +0,0 @@
-import os
-import streamlit_authenticator as stauth
-
-# Использование пароля из переменной окружения
-password = os.environ.get('MY_SECRET_PASSWORD', 'default_password')
-
-# Вывод в консоль хеша пароля 
-print(stauth.Hasher([password]).generate())
